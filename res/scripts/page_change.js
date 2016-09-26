0a84fde1238dfad29eb7f0483fa2f74f12874edd
Alec Waichunas <Alec Waichunas>
2016-09-26 18:10:29 -0500

    JS bugs added

--- res/scripts/page_change.js
+++ res/scripts/page_change.js
@@ -18,9 +18,42 @@ for(var i = 0; i < tabs.length; i++){
     //function for variable scope within click event
     (function(tab, link, startPos){
         tab.on("click", function(){
-            var minWidth = $("#content").css("min-width")
-            $("#content").css({"min-width": $("#content").width()})
-            $("body").css({"overflow-x": "hidden"})
+            var headerChildren = $("#content_header").children();
+            var bodyChildren = $("#content_body").children();
+
+            var minWidth = $("#content").css("min-width");
+            $("#content").css({"min-width": $("#content").width()});
+            $("body").css({"overflow-x": "hidden"});
+
+            $("#content_header").animate({
+                "margin-left": $(document).width()
+            }, 500);
+
+            var moveChildren = function(leftAmount, children, time){
+                for(var c = 0; c < children.length; c++){
+                    children[i].css({"position": "relative"});
+                    children[i].animate({
+                        "padding-left": leftAmount
+                    }, time);
+                }
+            };
+
+            /*moveChildren($(document).width(), headerChildren, 500);
+            moveChildren($(document).width(), bodyChildren, 500);
+
+            setTimeout(function(){
+                $("#content").empty();
+                $("#content").load(link);
+                moveChildren($(document).width(), $("#content_header").children(), 0);
+                moveChildren($(document).width(), $("#content_body").children(), 0);
+                moveChildren(startPos, $("#content_header").children(), 500);
+                moveChildren(startPos, $("#content_body").children(), 500);
+                setTimeout(function(){
+                    $("#content").css({"min-width": minWidth})
+                    $("body").css({"overflow-x": "auto"})
+                }, 500)
+            }, 500)
+            /*
             $("#content").animate({
                 left: $(document).width(),
             }, 500, function(){
@@ -29,10 +62,8 @@ for(var i = 0; i < tabs.length; i++){
                 $("#content").animate({
                     left: startPos
                 }, 500, function(){
-                    $("#content").css({"min-width": minWidth})
-                    $("body").css({"overflow-x": "auto"})
                 });
-            });
+            });*/
         });
     })(tabs[i][0], filePath + tabs[i][1], $content.position().left);
 }
